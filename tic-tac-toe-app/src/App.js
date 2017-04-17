ed0470cb956f065e4bb0ef1618fa7849f8f2ad5f
captrat <jpatricklogan@gmail.comgit config user.name>
2017-04-17 17:09:48 -0600

    feat(logic) Working TicTacToe

--- tic-tac-toe-app/src/App.js
+++ tic-tac-toe-app/src/App.js
@@ -1,30 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   constructor(props) {
     super(props)
-    // this.state = {
-    //   msg: "Hello, Puppies"
-    // }
-  }
-
-  componentWillMount(){
     this.state = {
-      msg: 'Yo Yo, World!'
+      player_x: 'x',
+      player_o: 'o',
+      player: 'x',
+      winner: null,
+      board: [
+        '', '', '', '', '', '', '', '', ''
+      ]
     }
   }
+
+  checkPlay () {
+    const player = this.state.player
+    const cells = this.state.board
+    const wins =[[0,1,2], [3,4,5], [6,7,8],
+                  [0,3,6], [1,4,7], [2,5,8],
+                  [0,4,8], [2,4,6]]
+
+
+  return wins.find(combo => {
+    if (cells[combo[0]] !== "" && cells[combo[1]] !== "" && cells[combo[2]]  !== "" &&
+        cells[combo[0]] === cells[combo[1]] && cells[combo[1]] === cells[combo[2]]) {
+          console.log('Player '+player + ' is a winner');
+          return player
+        }
+        else {
+          return false
+      }
+    })
+  }
+
+  handleClick(index) {
+    if(this.state.board[index] === '') {
+      this.state.board[index] = this.state.player
+      this.setState({
+        board: this.state.board,
+        player: this.state.player_x === this.state.player ?
+        this.state.player_o : this.state.player_x,
+        winner: this.checkPlay() ? this.state.player : null
+      })
+    }
+    console.log(index);
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2 onClick={() => this.setState({msg: this.state.msg + '!'})}>{this.state.msg}</h2>
+        <div className="board">
+          {this.state.board.map((cell, index) => {
+            return (
+            <div className="cell" key={index}
+            data-cell-id={index} onClick={() => {
+              this.handleClick(index)}}>
+              {cell}
+            </div>
+          )
+        })}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          { this.state.winner ?
+            <h1 className="winner">{`The winner is ${this.state.winner}`}
+            </h1> : null
+          }
       </div>
     );
   }
